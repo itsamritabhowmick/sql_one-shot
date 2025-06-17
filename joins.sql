7173b5ebb13d711b15fd12e16cc9e44fd60e5e4c
Amrita Bhowmick <milanbhwmick12341234@gmail.com>
2025-06-17 18:26:26 +0530

    All over description about Joins and UNION

--- joins.sql
+++ joins.sql
@@ -115,3 +115,14 @@ ON a.id = b.manager_id;
 
 
 
+/* ....................................UNION.................................*/
+
+SELECT name FROM employee
+UNION 
+SELECT name FROM employee; /* ...UNION didn't gave duplicate values */
+
+SELECT name FROM employee
+UNION ALL
+SELECT name FROM employee; /* ...UNION ALL gave duplicate values */
+
+
